5795384817942968875b9b1d96e79454c6523503
omar-besbes <86571415+omar-besbes@users.noreply.github.com>
2023-04-08 17:28:19 +0100

    feat: packer & kanagawa theme

---
+++ lua/plugins.lua
@@ -0,0 +1,12 @@
+-- Only required if you have packer configured as `opt`
+vim.cmd.packadd('packer.nvim')
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+	-- Get kanagawa theme
+	use 'rebelot/kanagawa.nvim'
+
+end)
+
