528b4031730fb7314e1e68d0e71d07b35812726c
omar-besbes <86571415+omar-besbes@users.noreply.github.com>
2023-04-09 01:08:23 +0100

    feat: LSP

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -28,5 +28,27 @@ return require('packer').startup(function(use)
 	-- Get git wrapper
 	use 'tpope/vim-fugitive'
 
+	-- Get LSP
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{                                      -- Optional
+				'williamboman/mason.nvim',
+				run = function()
+				pcall(vim.cmd, 'MasonUpdate')
+				end,
+			},
+			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},     -- Required
+			{'hrsh7th/cmp-nvim-lsp'}, -- Required
+			{'L3MON4D3/LuaSnip'},     -- Required
+		}
+	}
+
 end)
 
