f20387af6f64e850723bc3a7b65ae24717871ec6
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2023-11-08 20:20:49 +0100

    chore: add some keymaps to telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,19 @@
+-- Key mappings
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fp', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files,		{})
+vim.keymap.set('n', '<leader>fp', builtin.git_files,		{})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep,		{})
+vim.keymap.set('n', '<leader>fb', builtin.buffers,			{})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags,		{})
+vim.keymap.set('n', '<leader>fc', builtin.colorscheme,	{})
+
+-- Telescope setup
+require("telescope").setup({
+	pickers = {
+    find_files = {
+      theme = "ivy",
+    }
+  },
+})
 
