be5e294d9f94a30509478801f604792c3cf12cfd
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2023-11-24 01:14:46 +0100

    feat: diagnostics in status line
    fix filename in terminal

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,29 +1,40 @@
 require('lualine').setup({
 	options = {
-    component_separators = '|',
-    section_separators = { left = '', right = '' },
-  },
-  sections = {
-    lualine_a = {
-      { 'mode', separator = { left = '' }, right_padding = 2 },
-    },
-    lualine_b = { 'filename', 'branch' },
-    lualine_c = { 'diff' },
-    lualine_x = { 'filesize' },
-    lualine_y = { 'filetype', 'progress' },
-    lualine_z = {
-      { 'location', separator = { right = '' }, left_padding = 2 },
-    },
-  },
-  inactive_sections = {
-    lualine_a = { 'filename' },
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = { 'location' },
-  },
-  tabline = {},
-  extensions = {},
+		component_separators = '|',
+		section_separators = { left = '', right = '' },
+	},
+	sections = {
+		lualine_a = {
+			{ 'mode', separator = { left = '' }, right_padding = 2 },
+		},
+		lualine_b = {
+			-- TODO: find a better way to write this
+			'%{&ft == "toggleterm" ? "terminal (".b:toggle_number.")" : expand("%:t")}',
+			'branch'
+		},
+		lualine_c = {
+			'diff'
+		},
+		lualine_x = {
+			{ 'diagnostics', sources = { 'nvim_lsp' } }
+		},
+		lualine_y = {
+			'filetype',
+			'progress'
+		},
+		lualine_z = {
+			{ 'location', separator = { right = '' }, left_padding = 2 },
+		},
+	},
+	inactive_sections = {
+		lualine_a = { 'filename' },
+		lualine_b = {},
+		lualine_c = {},
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = { 'location' },
+	},
+	tabline = {},
+	extensions = {},
 })
 
